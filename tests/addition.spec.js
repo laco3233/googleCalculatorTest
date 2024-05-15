a7a10e1e1dfd66fac150110837848e9e46a3fd60
laco3233 <lacombetrevor@gmail.com>
2024-05-15 13:20:12 -0700

    Completed Addition Function Testing

--- tests/addition.spec.js
+++ tests/addition.spec.js
@@ -3,33 +3,64 @@
 const { test, expect } = require('@playwright/test');
 const { googlePage } = require('./support/pageobjectmodel/pages/google.page');
 
-test.afterEach(async ({ page }) => {
-    await page.getByText('AC', { exact: true }).click();
-})
+//Data Generation
+const num = [1,2,3,4,5,6,7,8,9,0];
+
+let a = Math.floor(Math.random() * num.length);
+let b = Math.floor(Math.random() * num.length);
+
+let makeDifferent = () => {
+    if (b === a) {
+        b += 1;
+    }
+};
+makeDifferent();
+
+let c = parseFloat(String(`${a}.${b}`));
+let d = parseFloat(String(`${b}.${a}`));
 
 //Tests to verify addition functionality
-const num = [1,2,3,4,5,6,7,8,9,0];
+
+test.afterEach(async ({ page }) => {
+    await page.getByText('AC', { exact: true }).click();
+    await expect(page.locator('xpath=//*[@id="cwos"]').first()).toHaveText('0'); 
+})
 
 for (const n of num) {
     test(`should add ${n} to ${n}`, async ({ page }) => {
         const google = new googlePage(page);
         await google.goto();
         await google.searchFor("calculator");
-        await page.getByRole('button', { name: `${n}` }).click();
-        await page.getByText('+').click();
-        await page.getByRole('button', { name: `${n}` }).click();
+        await page.getByRole('button', { name: `${n}`, exact: true }).click();
+        await page.getByText('+', { exact: true }).click();
+        await page.getByRole('button', { name: `${n}`, exact: true }).click();
         await page.getByLabel('equals').click();
         await expect(page.locator('xpath=//*[@id="cwos"]').first()).toHaveText(`${n + n}`);
     });
 }
 
-test('should match non matching numbers', async ({ page }) => {
+test('should add non matching numbers', async ({ page }) => {
     const google = new googlePage(page);
     await google.goto();
     await google.searchFor("calculator");
-    await page.getByRole('button', { name: '1' }).click();
+    await page.getByRole('button', { name: `${a}`, exact: true }).click();
     await page.getByText('+').click();
-    await page.getByRole('button', { name: '2' }).click();
+    await page.getByRole('button', { name:`${b}`, exact: true }).click();
     await page.getByLabel('equals').click();
-    await expect(page.locator('xpath=//*[@id="cwos"]').first()).toHaveText('3');
+    await expect(page.locator('xpath=//*[@id="cwos"]').first()).toHaveText(`${a + b}`);
+});
+
+test('should add numbers with decimals', async ({ page }) => {
+    const google = new googlePage(page);
+    await google.goto();
+    await google.searchFor("calculator");
+    await page.getByRole('button', { name: `${a}`, exact: true }).click();
+    await page.getByLabel('point').click();
+    await page.getByRole('button', { name:`${b}`, exact: true }).click();
+    await page.getByText('+').click();    
+    await page.getByRole('button', { name:`${b}`, exact: true }).click();
+    await page.getByLabel('point').click();
+    await page.getByRole('button', { name: `${a}`, exact: true }).click();
+    await page.getByLabel('equals').click();
+    await expect(page.locator('xpath=//*[@id="cwos"]').first()).toHaveText(`${c + d}`);
 });
